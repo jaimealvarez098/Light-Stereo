3e98aac2ecde2f9800498cbd26ea9f1835eb32af
Jaime Alvarez <jaimealvarez098@gmail.com>
2020-09-05 16:07:47 -0700

    This file contains the frequencies of each chord

---
+++ chords.py
@@ -0,0 +1,83 @@
+#keys 1
+C1 = 33
+C_sharp1 = 35
+D1 = 37
+D_sharp1 = 39
+E1 = 41
+F1 = 44
+F_sharp1 = 46
+G1 = 49
+G_sharp1 = 52
+A1 = 55
+A_sharp1 = 58
+B1 = 62
+
+#keys 2
+C2 = 65
+C_sharp2 = 69
+D2 = 73
+D_sharp2 = 78
+E2 = 82
+F2 = 87
+F_sharp2 = 93
+G2 = 98
+G_sharp2 = 104
+A2 = 110
+A_sharp2 = 117
+B2 = 123
+
+#key 3s
+C3 = 131
+C_sharp3 = 139
+D3 = 147
+D_sharp3 = 156
+E3 = 165
+F3 = 175
+F_sharp3 = 185
+G3 = 196
+G_sharp3 = 208
+A3 = 220
+A_sharp3 = 233
+B3 = 247
+
+#keys 4
+C4 = 262
+C_sharp4 = 277
+D4 = 294
+D_sharp4 = 311
+E4 = 330
+F4 = 349
+F_sharp4 = 370
+G4 = 392
+G_sharp4 = 415
+A4 = 440
+A_sharp4 = 466
+B4 = 494
+
+#keys 5
+C5 = 523
+C_sharp5 = 554
+D5 = 587
+D_sharp5 = 622
+E5 = 659
+F5 = 699
+F_sharp5 = 740
+G5 = 784
+G_sharp5 = 831
+A5 = 880
+A_sharp5 = 932
+B5 = 988
+
+#keys 6
+C6 = 1047
+C_sharp6 = 1109
+D6 = 1175
+D_sharp6 = 1245
+E6 = 1319
+F6 = 1397
+F_sharp6 = 1480
+G6 = 1568
+G_sharp6 = 1661
+A6 = 1760
+A_sharp6 = 1865
+B6 = 1976
